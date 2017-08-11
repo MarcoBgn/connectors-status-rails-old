0e3f0e4f311142322b5ab4c07753d0e4c97fb49b
marcocode <admin@hecticmojo.com>
2017-08-11 15:33:52 +0100

    Add request to /version

--- app/assets/javascripts/controller/home.js
+++ app/assets/javascripts/controller/home.js
@@ -7,6 +7,31 @@ angular
     $scope.statusProd = [];
     $scope.statusUAT = [];
 
+    $scope.appReference = {
+      'connector-shopify': 'universal-monkey-1',
+      'connector-keypay': 'compatible-wombat-3',
+      'connector-office365': 'visionary-eagle-8',
+      'connector-pipedrive': 'inverse-wombat-5',
+      'connector-sageone-global': 'versatile-skate-7',
+      'connector-neto': 'upgradable-salmon-5',
+      'connector-workflowmax': 'clueless-caterpillar-4',
+      'connector-eventbrite': 'monitored-koala-2',
+      'connector-freeagent': 'enhanced-falcon-1',
+      'connector-sageone-south': 'digitized-koala-8',
+      'connector-placetel': 'implemented-robot-1',
+      'connector-basecrm': 'customizable-teapot-4',
+      'connector-kounta': 'enhanced-monkey-9',
+      'connector-unleashed': 'cloned-cat-8',
+      'connector-constant-contact': 'focused-goat-3',
+      'connector-salesforce': 'visionary-wombat-3',
+      'connector-vend': 'managed-tiger-8',
+      'connector-moneybird': 'networked-raptor-9',
+      'connector-cin7': 'programmable-whale-4',
+      'connector-google-apps-for-work': 'intuitive-turkey-4',
+      'connector-hubspot': 'devolved-balloon-4',
+      'connector-sageone-north': 'reduced-turkey-1'
+        }
+
     // Not implemented yet
     $scope.perPage = 0;
 
@@ -20,12 +45,15 @@ angular
       })
       $scope.checkStatusProd = function (appName) {
         RepoFetcherService.getStatus(appName, 'Prod').then(function (response) {
-          var frameworkVersion = response.split('\n')[0];
-          self.statusProd.push(frameworkVersion);
+          var frameworkVersion = response.split('\n')[0].slice(18);
+          $scope.statusProd.push(frameworkVersion);
         });
       }
-      // .catch(function (response) {
-      //   $scope.refreshNotice = response;
-      // });
+      $scope.checkStatusUAT = function (appName) {
+        RepoFetcherService.getStatus($scope.appReference[appName], 'UAT').then(function (response) {
+          var frameworkVersion = response.split('\n')[0].slice(18);
+          $scope.statusUAT.push(frameworkVersion);
+        });
+      }
     }
   })
